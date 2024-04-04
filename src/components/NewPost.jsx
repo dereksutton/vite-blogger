dca8df6aee9157b5c29ce9b183465b67127e8052
Derek Sutton <dereksutton86@gmail.com>
2024-04-04 19:45:53 -0400

    added component for creating a new post

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import classes from './NewPost.module.css';
+
+const NewPost = () => {
+  const changeBodyHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+      </p>
+      <p>
+
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 const PostsList = () => {
     return (
-      <ul className={classes.posts}>
-        <Post author="Derek" body="React.js is awesome!" />
-        <Post author="Nicole" body="Check out the full course!" />
-      </ul>
+      <>
+        <NewPost />
+        <ul className={classes.posts}>
+          <Post author="Derek" body="React.js is awesome!" />
+          <Post author="Nicole" body="Check out the full course!" />
+        </ul>
+      </>
     );
 }
 
