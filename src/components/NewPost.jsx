91bafb87df5dc318f104ef12af1c0f9c54702b0e
Derek Sutton <dereksutton86@gmail.com>
2024-04-05 21:29:25 -0400

    added a state hook in the NewPost component to update the post content in real-time

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 const NewPost = () => {
-  const changeBodyHandler = (event) => {
-    console.log(event.target.value);
+  const [ enteredBody, setEnteredBody ] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -12,9 +14,7 @@ const NewPost = () => {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
-      <p>
-
-      </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
