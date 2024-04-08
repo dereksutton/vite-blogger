0498aa72bd16f90a41dfcf32a36e20f7ad01602e
Derek Sutton <dereksutton86@gmail.com>
2024-04-07 20:57:01 -0400

    lifted state up for post author and body content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
+
 import Post from './Post';
 import NewPost from './NewPost';
 import classes from './PostsList.module.css';
 
 const PostsList = () => {
-    return (
-      <>
-        <NewPost />
-        <ul className={classes.posts}>
-          <Post author="Derek" body="React.js is awesome!" />
-          <Post author="Nicole" body="Check out the full course!" />
-        </ul>
-      </>
-    );
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Nicole" body="Check out the full course!" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
