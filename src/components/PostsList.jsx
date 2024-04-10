a9e6bf7de17b67c5271d31fe44670805fe5ad00f
Derek Sutton <dereksutton86@gmail.com>
2024-04-09 21:23:36 -0400

    added Modal component to conditionally render the NewPost component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 
 import Post from './Post';
 import NewPost from './NewPost';
+import Modal from './Modal';
 import classes from './PostsList.module.css';
 
 const PostsList = () => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
 
@@ -16,9 +18,18 @@ const PostsList = () => {
     setEnteredAuthor(event.target.value);
   }
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      {modalIsVisible ? <Modal onClose={hideModalHandler}>
+        <NewPost 
+          onBodyChange={bodyChangeHandler} 
+          onAuthorChange={authorChangeHandler} 
+        />
+      </Modal> : null}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Nicole" body="Check out the full course!" />
